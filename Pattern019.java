37de8ff353039c31a715b2a403c3a8b93eca3fba
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-05 22:52:57 +0530

    Create Pattern019.java

---
+++ Pattern019.java
@@ -0,0 +1,27 @@
+// Mirrored Empty right Triangle Pattern
+import java.util.Scanner;
+
+public class Pattern019{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= (n-i); j++){
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= i; k++){
+                if(k == 1 || k == i || i == n){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+              System.out.println();  
+        }
+
+        }
+    }
+
+
