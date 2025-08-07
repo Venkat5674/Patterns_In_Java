ba993f6b7accc440c56d881c890e5b948c1a1285
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-07 23:20:00 +0530

    Create TreePattern.java

---
+++ TreePattern.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class TreePattern{
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = i; j < n; j++){
+               System.out.print(" ");
+                }
+            for(int j = 1; j <= (2*i-1); j++){
+                System.out.print("*");
+            }    
+            System.out.println();
+        }
+        
+       for(int i = 0; i < 2; i++){
+           for(int j = 0; j < n - 2; j++){
+               System.out.print(" ");
+           }
+           System.out.println("***");
+       }
+        
+    }
+}
