2190bbcac18b41e1d3f40eb29471c24e02c14872
Venkatesh Pamudurti <136252885+Venkat5674@users.noreply.github.com>
2025-08-14 22:59:49 +0530

    Create NumberDiamond.java

---
+++ NumberDiamond.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Pattern11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        // Upper half
+        for (int i = 1; i <= rows; i++) {
+            for (int s = 1; s <= rows - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int s = 1; s <= rows - i; s++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
